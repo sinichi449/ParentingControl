5b97725dc5b5fd4c1d4b6938f41000dc9dbe5f4b
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-08-20 12:54:50 +0700

    initial commit

--- app/src/main/java/com/sinichi/parentingcontrol/MainActivity.java
+++ app/src/main/java/com/sinichi/parentingcontrol/MainActivity.java
@@ -1,14 +1,33 @@
 package com.sinichi.parentingcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.sinichi.parentingcontrol.recycleadapter.RvAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private RvAdapter rvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+        initRecyclerView();
+    }
+
+    private void initComponents() {
+        rv = findViewById(R.id.rv_catatan);
+    }
+
+    private void initRecyclerView() {
+        rv.setAdapter(rvAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/sinichi/parentingcontrol/recycleadapter/RvAdapter.java
@@ -0,0 +1,36 @@
+package com.sinichi.parentingcontrol.recycleadapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sinichi.parentingcontrol.R;
+
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
