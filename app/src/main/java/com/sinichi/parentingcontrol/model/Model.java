a780bf3af42be0fe8b936b95d184cfc7aa3f41b0
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-08-20 14:09:34 +0700

    c commit

--- app/src/main/java/com/sinichi/parentingcontrol/model/Model.java
+++ app/src/main/java/com/sinichi/parentingcontrol/model/Model.java
@@ -5,4 +5,34 @@ public class Model {
     String jumlahSholat;
     boolean membantuOrangTua;
     boolean sekolah;
+
+    public Model(String jumlahSholat, boolean membantuOrangTua, boolean sekolah) {
+        this.jumlahSholat = jumlahSholat;
+        this.membantuOrangTua = membantuOrangTua;
+        this.sekolah = sekolah;
+    }
+
+    public String getJumlahSholat() {
+        return jumlahSholat;
+    }
+
+    public void setJumlahSholat(String jumlahSholat) {
+        this.jumlahSholat = jumlahSholat;
+    }
+
+    public boolean isMembantuOrangTua() {
+        return membantuOrangTua;
+    }
+
+    public void setMembantuOrangTua(boolean membantuOrangTua) {
+        this.membantuOrangTua = membantuOrangTua;
+    }
+
+    public boolean isSekolah() {
+        return sekolah;
+    }
+
+    public void setSekolah(boolean sekolah) {
+        this.sekolah = sekolah;
+    }
 }
