6c9734c0af8da672abef8d63777a8dd54280af23
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-08-20 14:25:47 +0700

    d commit

--- app/src/main/java/com/sinichi/parentingcontrol/recycleadapter/RvAdapter.java
+++ app/src/main/java/com/sinichi/parentingcontrol/recycleadapter/RvAdapter.java
@@ -3,13 +3,24 @@ package com.sinichi.parentingcontrol.recycleadapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sinichi.parentingcontrol.R;
+import com.sinichi.parentingcontrol.model.Model;
+
+import java.util.List;
 
 public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
+
+    List<Model> dataList;
+
+    public RvAdapter(List<Model> dataList) {
+        this.dataList = dataList;
+    }
 
     @NonNull
     @Override
@@ -20,17 +31,26 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Model model = dataList.get(position);
+        holder.tvJumlahSholat.setText(model.getJumlahSholat());
+        holder.chkMembantuOrtu.setChecked(model.isMembantuOrangTua());
+        holder.chkSekolah.setChecked(model.isSekolah());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dataList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvJumlahSholat;
+        private CheckBox chkMembantuOrtu, chkSekolah;
+
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvJumlahSholat = itemView.findViewById(R.id.tv_jumlahSholat);
+            chkMembantuOrtu = itemView.findViewById(R.id.chkbx_membantuOrtu);
+            chkSekolah = itemView.findViewById(R.id.chkbx_sekolah);
         }
     }
 }
