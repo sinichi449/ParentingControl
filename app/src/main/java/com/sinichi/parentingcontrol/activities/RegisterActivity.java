f3a1ab346ca17705324aa7134c5dec3c8d4fa50c
Rafanec <fandiahmad815@gmail.com>
2019-08-24 11:08:12 +0700

    h commit

--- app/src/main/java/com/sinichi/parentingcontrol/activities/RegisterActivity.java
+++ app/src/main/java/com/sinichi/parentingcontrol/activities/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sinichi.parentingcontrol.R;
@@ -13,6 +14,7 @@ import com.sinichi.parentingcontrol.R;
 public class RegisterActivity extends AppCompatActivity {
 
     private Button btnRegister;
+    private TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +33,20 @@ public class RegisterActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Authenticate user input
+
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     private void initComponents() {
+        tvLogin = findViewById(R.id. tv_sudahPunyaAkun);
         btnRegister = findViewById(R.id.btn_register);
     }
 }
