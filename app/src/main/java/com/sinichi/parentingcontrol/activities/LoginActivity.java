a12f1497275667d60c875335bec398318dc67e90
Bagus Eka Saputra <bagusekasaputra460@gmail.com>
2019-08-24 10:29:16 +0700

    added firebase

--- app/src/main/java/com/sinichi/parentingcontrol/activities/LoginActivity.java
+++ app/src/main/java/com/sinichi/parentingcontrol/activities/LoginActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sinichi.parentingcontrol.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button btnLogin;
+    private TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +26,26 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //TODO: Authenticate user data
+
                 Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Authenticate user input
+
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(i);
             }
         });
     }
 
     private void initComponents() {
-        btnLogin = findViewById(R.id.btn_login);
+        btnLogin = findViewById(R.id.btn_register);
+        tvRegister = findViewById(R.id.tv_register);
     }
 }
